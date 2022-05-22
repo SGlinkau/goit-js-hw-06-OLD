8986c09032a28a6653e3085c1b1fd5f5c0061c7e
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2022-05-22 11:13:54 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,6 @@
 const inputArea = document.querySelector("#validation-input");
 inputArea.addEventListener("blur", () => {
-    if (inputArea.value.length >= 6) {
+    if (inputArea.value.length === 6) {
         inputArea.classList.add("valid");
         inputArea.classList.remove("invalid");
     } else {
