22e0e16150d544766290324c3244b1c06074e090
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2022-07-24 12:30:13 +0200

    Update task-02.js
    Poprawki zadania 2

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,22 @@ const ingredients = [
 ];
 
 const list = document.querySelector("#ingredients");
+  
+  // for (let i = 0; i < ingredients.length; i++) {
+  //   let listItem = document.createElement("li");
+  //   listItem.classList.add("item");
+  //   listItem.innerHTML = ingredients[i];
+  //   const ulIngredients = [];
+  //   ulIngredients.push(listItem);
+  //   console.log(ulIngredients);
+  // };
+  
+  // list.append(...ulIngredients);
 
-for (let i = 0; i < ingredients.length; i++) {
-  let listItem = document.createElement("li");
-  listItem.classList.add("item");
-  listItem.innerHTML = ingredients[i];
-  list.appendChild(listItem);
-};
+  const ulIngredients = [];
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  ulIngredients.push(listItem);
+});
+list.append(...ulIngredients);
